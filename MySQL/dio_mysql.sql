42d18d805d6add69607f2f93260a4f0d24b56b47
Welberth <welberth_henrique@hotmail.com>
2022-07-19 16:05:50 -0300

    MySQL commands 2

--- MySQL/dio_mysql.sql
+++ MySQL/dio_mysql.sql
@@ -14,3 +14,26 @@ INSERT INTO people (name, birth) VALUES (
     'Linn March',
     '1992/08/03'
 );
+INSERT INTO people (name, birth) VALUES (
+    'Mark Bill',
+    '1972/12/26'
+);
+
+SELECT name FROM people;
+
+UPDATE people SET name = 'Liniker March' WHERE id = 2;
+
+
+SELECT * FROM people WHERE id = 3;
+DELETE FROM people WHERE id = 3;
+
+SELECT * FROM people ORDER BY name;
+SELECT * FROM people ORDER BY name DESC;
+
+ALTER TABLE people ADD gender VARCHAR(1) NOT NULL AFTER birth;
+UPDATE people SET gender = 'M' WHERE id = 1;
+UPDATE people SET gender = 'F' WHERE id = 2;
+UPDATE people SET gender = 'M' WHERE id = 3;
+
+SELECT * FROM people GROUP BY gender;
+SELECT COUNT(id), gender FROM people GROUP BY gender;
